ee07bacd74ce7b92ec1e43449897a712019b2859
nathva <nvelasquez.19@est.ucab.edu.ve>
2024-10-03 16:21:38 -0400

    feat: :sparkles: create base project

---
+++ compound_feature_brick/android/app/src/main/kotlin/com/example/verygoodcore/compound/feature/brick/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.compound_feature_brick
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ compound_feature_brick/lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:compound_feature_brick/app/app.dart';
+import 'package:compound_feature_brick/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
