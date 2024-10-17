22ee7aae17ffc62173626ccfb28ef542cb2b234f
nathva <nvelasquez.19@est.ucab.edu.ve>
2024-10-17 16:28:57 -0400

    feat: :sparkles: stepper feature

--- brick/feature_brick_plus/__brick__/{{feature_name.snakeCase()}}/bloc/{{feature_name.snakeCase()}}_bloc.dart
+++ brick/feature_brick_plus/__brick__/{{feature_name.snakeCase()}}/bloc/{{feature_name.snakeCase()}}_bloc.dart
@@ -8,5 +8,39 @@ part '{{feature_name.snakeCase()}}_state.dart';
 
 class {{feature_name.pascalCase()}}Bloc extends Bloc<{{feature_name.pascalCase()}}Event, {{feature_name.pascalCase()}}State> {
   {{feature_name.pascalCase()}}Bloc() : super(const {{feature_name.pascalCase()}}State()) {
+  {{#isStepper}} on<{{feature_name.pascalCase()}}NextStep>(_on{{feature_name.pascalCase()}}NextStep);
+    on<{{feature_name.pascalCase()}}PreviousStep>(
+      _on{{feature_name.pascalCase()}}PreviousStep,
+    );{{/isStepper}}
   }
+  {{#isStepper}}FutureOr<void> _on{{feature_name.pascalCase()}}NextStep(
+    {{feature_name.pascalCase()}}NextStep event,
+    Emitter<{{feature_name.pascalCase()}}State> emit,
+  ) async {
+    final nextIndex = state.currentPageIndex + 1;
+    final nextStep = event.nextStep ?? state.step.nextStep;
+
+      emit(
+        state.copyWith(
+          currentPageIndex: nextIndex,
+          step: nextStep,
+        ),
+      );
+  }
+
+  FutureOr<void> _on{{feature_name.pascalCase()}}PreviousStep(
+    {{feature_name.pascalCase()}}PreviousStep event,
+    Emitter<{{feature_name.pascalCase()}}State> emit,
+  ) async {
+    final previousIndex = state.currentPageIndex - 1;
+    final previousStep = state.step.previousStep;
+
+      emit(
+        state.copyWith(
+          currentPageIndex: previousIndex,
+          step: previousStep,
+        ),
+      );
+  }
+  {{/isStepper}}
 }
--- brick/feature_brick_plus/__brick__/{{feature_name.snakeCase()}}/bloc/{{feature_name.snakeCase()}}_event.dart
+++ brick/feature_brick_plus/__brick__/{{feature_name.snakeCase()}}/bloc/{{feature_name.snakeCase()}}_event.dart
@@ -4,6 +4,20 @@ abstract class {{feature_name.pascalCase()}}Event extends Equatable {
   const {{feature_name.pascalCase()}}Event();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 
 }
+{{#isStepper}}class {{feature_name.pascalCase()}}NextStep extends {{feature_name.pascalCase()}}Event {
+  const {{feature_name.pascalCase()}}NextStep({
+    this.nextStep,
+  });
+
+  final {{feature_name.pascalCase()}}Step? nextStep;
+
+  @override
+  List<Object?> get props => [nextStep];
+}
+
+class {{feature_name.pascalCase()}}PreviousStep extends {{feature_name.pascalCase()}}Event {
+  const {{feature_name.pascalCase()}}PreviousStep();
+}{{/isStepper}}
